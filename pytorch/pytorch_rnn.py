b671cb0567d65a9b154c9e8b823b6aacaf4dad2b
Xiang Zhang <zhan410@usc.edu>
2019-02-25 03:40:01 -0800

    pytorch RNN update

--- pytorch/pytorch_rnn.py
+++ pytorch/pytorch_rnn.py
@@ -1,13 +1,14 @@
 import torch
 import time
 
-rnn = torch.nn.RNN(150528, 32, 50)
-input = torch.randn(20, 1, 150528)
-h0 = torch.randn(50, 1, 32)
+rnn = torch.nn.RNN(150528, 32, 1)
 
 t = time.time()
-output, hn = rnn(input, h0)
-print('pytorch rnn: ', (time.time()-t)*1000)
+for i in range(100):
+    input = torch.randn(1, 1, 150528)
+    h0 = torch.randn(1, 1, 32)
+    output, hn = rnn(input, h0)
+print('pytorch rnn: ', (time.time()-t)*100) # 140.5630111694336
 
 print (output.shape)
 torch.onnx.export(rnn, input, "pytorch_rnn.onnx")
